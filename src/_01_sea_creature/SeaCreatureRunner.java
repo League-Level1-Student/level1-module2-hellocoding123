bf295d766fb53d3e9af9a93fde1c271dd451849b
league <league@localhost>
2023-03-18 12:59:27 -0700

    sea creature :sunglasses:

---
+++ src/_01_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,18 @@
+package _01_sea_creature;
+
+public class SeaCreatureRunner {
+	static public void main(String[] args) {
+		SeaCreature spongebob = new SeaCreature("spongebob");
+		spongebob.getName();
+		spongebob.eat();
+		spongebob.laugh();
+		SeaCreature patrick = new SeaCreature("patrick");
+		patrick.getName();
+		patrick.eat();
+		patrick.laugh();
+		SeaCreature squidward = new SeaCreature("squidward");
+		squidward.getName();
+		squidward.eat();
+		squidward.laugh();
+	}
+}
